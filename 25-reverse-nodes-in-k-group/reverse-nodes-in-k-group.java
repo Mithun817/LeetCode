88750a33e28ab005b74c4db42b56b9fc4a1155ce
Mithun817 <engr.mithunrd@gmail.com>
2026-07-01 23:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (16.64%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -9,52 +9,65 @@
  * }
  */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode temp = head;
-
-        //Length
-        int n = 0;
-        while(temp!=null)
-        {
-            temp = temp.next;
-            n++;
-        }
-
-        //First Rotation
-        ListNode current = head;
+    public ListNode reverse(ListNode current , int k)
+    {
         ListNode prev = null;
-        int i=1;
-        while(i<=k)
+        while(k-->0)
         {
             ListNode next = current.next;
             current.next = prev;
             prev = current;
             current = next;
-            i++;
         }
-        ListNode newHead = prev;
-        ListNode nnext = head;
-        //Remaining Rotations
-        n -= k;
-        while(k<=n)
+        return prev;
+    }
+    public ListNode find(ListNode head , int k)
+    {
+        ListNode temp = head;
+        while(k-->1 && temp!=null)
         {
-            i=1;
-            ListNode curTail = current;
-            prev = null;
-            while(i<=k)
-            {
-                ListNode next = current.next;
-                current.next = prev;
-                prev = current;
-                current = next;
-                i++;
-            }
-            n -= k;
-            nnext.next = prev;
-            nnext = curTail;
+            temp = temp.next;
         }
-        nnext.next=current;
-        return newHead;
-
+        return temp;
+    }
+    public int length(ListNode head)
+    {
+        int n=0;
+        ListNode temp = head;
+        while(temp!=null)
+        {
+            temp = temp.next;
+            n++;
+        }
+        return n;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || k==1) return head;
+        int n = length(head);
+        if(n<k) return head;
+        ListNode temp = head;
+        ListNode prevNode = null;
+        while(temp != null)
+        {
+            ListNode kth = find(temp , k);
+            if(kth == null)
+            {
+                if(prevNode != null) prevNode.next = temp;
+                break;
+            }
+            ListNode nextNode = kth.next;
+            reverse(temp , k);
+            if(temp == head)
+            {
+                head = kth;
+            }
+            else
+            {
+                prevNode.next = kth;
+            }
+            prevNode = temp;
+            temp = nextNode;
+        }
+        return head;
     }
 }
