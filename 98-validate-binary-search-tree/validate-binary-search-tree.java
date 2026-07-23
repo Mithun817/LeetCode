3cdf2d9937913f31aad5e71deaaebebe1141a692
Mithun817 <engr.mithunrd@gmail.com>
2026-07-23 06:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (7.33%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,24 +15,14 @@
  */
 class Solution {
     ArrayList<Integer> list = new ArrayList<>();
-    int prev = Integer.MIN_VALUE+1;
     boolean flag = true;
     public void inorder(TreeNode root)
     {
         if(root == null) return;
-        // else if(list.size() > 0 && list.get(list.size()-1) >= root.val)
-        // {
-        //     //System.out.println("prev : "+prev +" val : "+root.val);
-        //     flag = false;
-        //     return;
-        // }
-
-        //System.out.println("prev : "+prev +" val : "+root.val);
 
         inorder(root.left);
         if(list.size() > 0 && list.get(list.size()-1) >= root.val)
         {
-            //System.out.println("prev : "+prev +" val : "+root.val);
             flag = false;
             return;
         }
