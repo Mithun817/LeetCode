98567f005023a06e58431a7260625ffb9d7d9e17
Mithun817 <engr.mithunrd@gmail.com>
2026-08-05 15:40:22 +0530

    Time: 147 ms (23.42%) | Memory: 66.6 MB (5.02%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        boolean[] arr = new boolean[right+1];
+        List<Integer> primes = new ArrayList<>();
+        int minn = Integer.MAX_VALUE , prev = 0;
+
+        for(int i=2 ; i*i<=right ; i++)
+        {
+            if(!arr[i])
+            {
+                for(int j=i*i ; j<=right ; j+=i)
+                {
+                    arr[j] = true;
+                }
+            }
+        }
+        for(int i=left ; i<=right ; i++)
+        {
+            if(i<2) continue;
+            if(!arr[i])
+            {
+                primes.add(i);
+                minn = Math.min(minn , i-prev);
+                prev = i;
+            }
+        }
+        System.out.println(primes);
+        for(int i=0 ; i<primes.size()-1 ; i++)
+        {
+            int n1 = primes.get(i);
+            int n2 = primes.get(i+1);
+            if(n2-n1 == minn) return new int[] {n1 , n2};
+        }
+        return new int[] {-1 , -1};
+    }
+}
